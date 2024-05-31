f2133546ea553cef62c0fbd07b06ca1f9be2611c
Mohak Waghchaure <mohak11jan@gmail.com>
2024-05-30 21:15:37 -0400

    toggle button

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Circles from './Circles';
 
 function Pricing(){
+    const [period, setPeriod] = useState('annually');
+    function showAnnually(){
+        document.querySelector('.btn-annually').classList.add("active");
+        document.querySelector('.btn-monthly').classList.remove("active");
+        setPeriod('annually');
+    }
+    function showMonthly(){
+        document.querySelector('.btn-monthly').classList.add("active");
+        document.querySelector('.btn-annually').classList.remove("active");
+        setPeriod('monthly');
+    }
     return(
         <Fragment>
             <div className='pricing-wrapper'>
@@ -13,7 +24,11 @@ function Pricing(){
                             <div className='sub-text'>Bring customer data to every business decision.</div>
                         </div>
                     </div>
-                    <div className='pricing-cards'>
+                    <div className='period-set-wrapper'>
+                        <button className='btn-annually active' onClick={showAnnually}>Annually</button>
+                        <button className='btn-monthly' onClick={showMonthly}>Monthly</button>
+                    </div>
+                    {(period == 'annually') && <div className='pricing-cards annually'>
                         <div className='row'>
                             <div className='col-4'>
                                 <div className='card card-wrapper'>
@@ -104,7 +119,99 @@ function Pricing(){
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>}
+                    {(period == 'monthly') && <div className='pricing-cards monthly'>
+                        <div className='row'>
+                            <div className='col-4'>
+                                <div className='card card-wrapper'>
+                                    <div className='card-body card'>
+                                        <div className='title'>Essential</div>
+                                        <div className='sub-text'>Lorem Ipsum is simply dummy text</div>
+                                        <div className='price-wrap'>
+                                            <div className='price'>$20k</div>
+                                            <div className='sm-text'>Billed monthly</div>
+                                            <div className='line-bottom'></div>
+                                        </div>
+                                        <div className='information-wrap'>
+                                            <div className='info'>What's include?</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>4,500 respondent completes</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Unlimited surveys</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Unlimited users</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Complete surveys in hours</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>50-2000 completes per survey</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>30 day quick start onboarding</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Max 15 questions per survey</div>
+                                            
+                                        </div>
+                                        <div className='book-demo-wrap'>
+                                            <div className='btn-wrapper'>
+                                                <button className='btn'>Book demo</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-4'>
+                                <div className='card card-wrapper'>
+                                    <div className='card-body card popular'>
+                                        <div className='title'>Essential Plus</div>
+                                        <div className='sub-text'>Lorem Ipsum is simply dummy text</div>
+                                        <div className='price-wrap'>
+                                            <div className='price'>$44k</div>
+                                            <div className='sm-text'>Billed monthly</div>
+                                            <div className='line-bottom'></div>
+                                        </div>
+                                        <div className='information-wrap'>
+                                            <div className='info'>What's include?</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>10,000 respondent completes</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Unlimited surveys</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Unlimited users</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Complete surveys in hours</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>50-2000 completes per survey</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>30 day quick start onboarding</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Max 15 questions per survey</div>
+                                            
+                                        </div>
+                                        <div className='book-demo-wrap'>
+                                            <div className='btn-wrapper'>
+                                                <button className='btn'>Book demo</button>
+                                            </div>
+                                        </div>
+                                        <div className='highlight-popular'>Popular</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-4'>
+                                <div className='card card-wrapper'>
+                                    <div className='card-body card'>
+                                        <div className='title'>Enterprise</div>
+                                        <div className='sub-text'>Lorem Ipsum is simply dummy text</div>
+                                        <div className='price-wrap'>
+                                            <div className='price'>$70k</div>
+                                            <div className='sm-text'>Billed monthly</div>
+                                            <div className='line-bottom'></div>
+                                        </div>
+                                        <div className='information-wrap'>
+                                            <div className='info'>What's include?</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>17,500 respondent completes</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Unlimited surveys</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Unlimited users</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Complete surveys in hours</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>50-2000 completes per survey</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>30 day quick start onboarding</div>
+                                            <div className='info-point'><span className='bullet-icon'></span>Max 15 questions per survey</div>
+                                            
+                                        </div>
+                                        <div className='book-demo-wrap'>
+                                            <div className='btn-wrapper'>
+                                                <button className='btn'>Book demo</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>}
                     <div className='get-started-wrapper'>
                         <div className='get-started'>
                             <div className='title'>Not sure where to start?</div>
